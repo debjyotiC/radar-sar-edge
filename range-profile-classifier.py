9f61d07d759a68b472529991588030671248c53d
Debjyoti Chowdhury <djbabai.debjyoti@gmail.com>
2024-04-22 20:59:53 +0530

    updates

--- range-profile-classifier.py
+++ range-profile-classifier.py
@@ -1,64 +1,62 @@
 import numpy as np
-from os import listdir
-from os.path import isdir, join
 import tensorflow as tf
 from sklearn.model_selection import train_test_split
+from tensorflow.keras import layers
 import matplotlib.pyplot as plt
 
-data = np.load("data/npz_files/umbc_outdoor_processed.npz", allow_pickle=True)
+load_data = np.load("data/npz_files/umbc_outdoor_processed.npz", allow_pickle=True)
 
-x_data = data['out_x']
-y_data = data['out_y']
+classes = 4
 
-print(x_data.shape)
+ground_mask = np.ones((9, 256))
+ground_mask[:, :10] = 0
 
-dataset_path = 'data/csv_files/umbc'
-
-all_targets = [target for target in listdir(dataset_path) if isdir(join(dataset_path, target))]
-classes = len(all_targets)
+x_data = load_data['out_x'] * ground_mask
+y_data = load_data['out_y']
 
 y_data = tf.keras.utils.to_categorical(y_data - 1, classes)
-x_train, x_test, y_train, y_test = train_test_split(x_data, y_data, train_size=0.6)
 
-x_train = tf.expand_dims(x_train, axis=-1)
+train_ratio = 0.70
+test_ratio = 1 - train_ratio
 
+x_train, x_test, y_train, y_test = train_test_split(x_data, y_data, test_size=1 - train_ratio)
 
 train_dataset = tf.data.Dataset.from_tensor_slices((x_train, y_train))
-validation_dataset = tf.data.Dataset.from_tensor_slices((x_test, y_test))
+test_dataset = tf.data.Dataset.from_tensor_slices((x_test, y_test))
 
+# Create the ANN model
 model = tf.keras.Sequential([
-    tf.keras.layers.Reshape((9, 256, 1), input_shape=x_train.shape[1:]),
-    tf.keras.layers.Conv2D(32, (2, 2), activation='relu', padding='same'),
-    tf.keras.layers.MaxPooling2D(2, 2),
-    tf.keras.layers.Conv2D(32, (2, 2), activation='relu', padding='same'),
-    tf.keras.layers.MaxPooling2D(2, 2),
-    tf.keras.layers.Conv2D(64, (2, 2), activation='relu', padding='same'),
-    tf.keras.layers.MaxPooling2D(2, 2),
-    tf.keras.layers.Dropout(0.2),
-    tf.keras.layers.Flatten(),
-    tf.keras.layers.Dense(128, activation='relu'),
-    tf.keras.layers.Dense(classes, activation='softmax')
+    layers.Flatten(input_shape=x_data[0].shape),
+    layers.Dense(128, activation='relu'),
+    layers.Dense(64, activation='relu'),
+    layers.Dense(64, activation='relu'),
+    layers.Dense(32, activation='relu'),
+    layers.Dense(classes, activation='softmax')
 ])
 
-# model.summary()
-model.compile(loss=tf.keras.losses.CategoricalCrossentropy(),
-              optimizer=tf.keras.optimizers.Adam(learning_rate=0.000001), metrics=['acc'])
+model.summary()
+model.compile(optimizer=tf.keras.optimizers.Adam(learning_rate=0.1), loss=tf.keras.losses.categorical_crossentropy,
+              metrics=['accuracy'])
 
 # this controls the batch size
-BATCH_SIZE = 10
+BATCH_SIZE = 40
 train_dataset = train_dataset.batch(BATCH_SIZE, drop_remainder=False)
-validation_dataset = validation_dataset.batch(BATCH_SIZE, drop_remainder=False)
+test_dataset = test_dataset.batch(BATCH_SIZE, drop_remainder=False)
 
-history = model.fit(train_dataset, epochs=1000, validation_data=validation_dataset)
+history = model.fit(train_dataset, epochs=100, validation_data=test_dataset)
 
-
-acc = history.history['acc']
-val_acc = history.history['val_acc']
+acc = history.history['accuracy']
+val_acc = history.history['val_accuracy']
 loss = history.history['loss']
 val_loss = history.history['val_loss']
 
 print(f"Training Accuracy: {round(np.average(acc), 3)}")
 print(f"Validation Accuracy: {round(np.average(val_acc), 3)}")
+
+predicted_labels = model.predict(x_test)
+actual_labels = y_test
+
+# np.savez('test-results/test-gray-results.npz', out_x=predicted_labels, out_y=actual_labels)
 
 epochs = range(1, len(acc) + 1)
 fig, axs = plt.subplots(2, 1)
